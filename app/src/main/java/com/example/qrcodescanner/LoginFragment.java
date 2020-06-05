8e0e0c51f48df52024c5d09d8756628a65deb011
suudupa <suhas.udupa@yahoo.ca>
2020-06-04 20:04:32 -0400

    Added simple login check

--- app/src/main/java/com/example/qrcodescanner/LoginFragment.java
+++ app/src/main/java/com/example/qrcodescanner/LoginFragment.java
@@ -5,12 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import java.util.Objects;
 
 public class LoginFragment extends Fragment {
+
+    private EditText usernameField;
+    private EditText passwordField;
 
     public LoginFragment() { }
 
@@ -23,6 +30,8 @@ public class LoginFragment extends Fragment {
         }
 
         View loginView = inflater.inflate(R.layout.fragment_login, container, false);
+        usernameField = loginView.findViewById(R.id.usernameField);
+        passwordField = loginView.findViewById(R.id.passwordField);
         Button loginBtn = loginView.findViewById(R.id.loginBtn);
 
         loginBtn.setOnClickListener(view -> login());
@@ -31,11 +40,34 @@ public class LoginFragment extends Fragment {
     }
 
     private void login() {
+        if (isSignInSuccessful(usernameField.getText().toString().trim(), passwordField.getText().toString().trim())) {
+            goToHome();
+        } else {
+            usernameField.getText().clear();
+            passwordField.getText().clear();
+        }
+    }
+
+    private boolean isSignInSuccessful(String username, String password) {
+        if (username.isEmpty() || password.isEmpty()) {
+            showToast(getString(R.string.loginFailText));
+            return false;
+        } else {
+            showToast(getString(R.string.loginSuccessText));
+            return true;
+        }
+    }
+
+    private void goToHome() {
         HomeFragment homeFragment = new HomeFragment();
         getChildFragmentManager().beginTransaction()
                 .replace(R.id.loginFragmentContainer, homeFragment, "homeFragment")
                 .addToBackStack(null)
                 .commit();
         SharedPreferences.INSTANCE.saveBoolean(Objects.requireNonNull(getContext()), SharedPreferences.isUserLoggedIn, true);
+    }
+
+    private void showToast(String value) {
+        Toast.makeText(getContext(), value, Toast.LENGTH_LONG).show();
     }
 }
