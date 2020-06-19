7e7c05a9ad1982b50ab0d05cc255b1dc5f939280
Daniel Bucci <danielbucci2@gmail.com>
2020-06-19 09:13:08 -0400

    successful http request functionality implemented

--- app/src/main/java/com/example/qrcodescanner/ResultsActivity.java
+++ app/src/main/java/com/example/qrcodescanner/ResultsActivity.java
@@ -5,26 +5,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.Headers;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 
 public class ResultsActivity extends AppCompatActivity {
 
     private TextView result;
     private Button scanAgain;
+    private String id;
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
 
     private final OkHttpClient client = new OkHttpClient();
-    private String userUrl = "https://api.darksky.net/forecast/c24d3d3cff53ca21cca995776dd43b06/37.8267,-122.4233";
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +41,53 @@ public class ResultsActivity extends AppCompatActivity {
 
         this.result = (TextView) findViewById(R.id.resultTxt);
         this.scanAgain = (Button) findViewById(R.id.scanAgainBtn);
-        int id = Integer.parseInt(getIntent().getStringExtra("RESULT"));
-        String username = "";
+        this.id = getIntent().getStringExtra("RESULT");
 
-        this.userUrl = "http://192.168.0.193/api/crmid/" + id;
 
-        this.result.setText(id+"");
+        this.url = "http://192.168.0.193/api/crmid";
+
+        this.result.setText(id);
+
+        JSONObject json = new JSONObject();
+
+        try{
+            json.put("id", Integer.parseInt(id));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
         try {
-            run();
+            post(this.url, json.toString(), new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    if(response.isSuccessful()){
+                        String responseData = response.body().string();
+                        System.out.println("Call successful"+responseData);
+                        try{
+                            System.out.println("Im where the call happens");
+                            JSONObject jsonObject = new JSONObject(responseData);
+                            setInfo(jsonObject);
+                        } catch (Exception e){
+                            System.out.println("Call went wrong" + e.getMessage());
+                            e.printStackTrace();
+                        }
+
+                    } else{
+                        System.out.println("Went wrong"+response.body().string());
+                    }
+                }
+            });
         } catch (Exception e){
-           e.printStackTrace();
+            System.out.println("Failure " + e.getMessage());
+            Toast.makeText(this, "Oops scan didnt work", Toast.LENGTH_LONG).show();
         }
+
+
 
         this.scanAgain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,30 +101,27 @@ public class ResultsActivity extends AppCompatActivity {
         startActivity(new Intent(this, ScannerActivity.class));
     }
 
-    public void run() throws Exception {
+    private Call post(String url, String json, Callback callback) {
+        RequestBody body = RequestBody.create(json, JSON);
         Request request = new Request.Builder()
-                .url(userUrl)
+                .url(url)
+                .post(body)
                 .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override public void onFailure(Call call, IOException e) {
-                e.printStackTrace();
-                System.out.println("Failure: " + e.getMessage());
-            }
-
-            @Override public void onResponse(Call call, Response response) throws IOException {
-                try (ResponseBody responseBody = response.body()) {
-                    if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-
-                    Headers responseHeaders = response.headers();
-                    for (int i = 0, size = responseHeaders.size(); i < size; i++) {
-                        System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-                    }
-
-                    System.out.println("Response Gotten: "+responseBody.string());
-                }
-            }
-        });
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+        return call;
     }
+
+    public void setInfo(JSONObject obj){
+        System.out.println("I got here" );
+        try {
+           System.out.println("NBasket: " + obj.getString("numberOfBaskets"));
+           this.result.setText(obj.getString("numberOfBaskets"));
+       } catch (Exception ex){
+           System.out.println("Yikes: " + ex.getMessage());
+           ex.printStackTrace();
+       }
+    }
+
 
 }
